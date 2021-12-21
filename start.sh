1e422f8c2d6a7e233dcdd3ee01051d1df94aac06
Rose Vlasova <nikarose@ya.ru>
2021-12-21 10:00:24 +0300

    fixed

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 set -x
 sudo apt-get update -y
 sudo apt-get install -y ansible git
+sudo ansible-galaxy collection install community.docker
 git clone https://github.com/Troitsa/devops_summary
-sudo ansible-playbook -i hosts/hosts.inv playbook.yml
+sudo ansible-playbook -i devops_summary/hosts/hosts.inv devops_summary/playbook.yml
